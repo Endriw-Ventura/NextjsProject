ee683798e52640c64219e637c9b623e6cffc6c33
Endriw Ventura <endriwventura@yahoo.com>
2024-12-04 14:26:58 -0300

    created project

---
+++ pages/faq.js
@@ -0,0 +1,10 @@
+import Link from "../src/components/Link";
+
+export default function HomePage() {
+  return (
+    <div>
+      <h1>FAQ</h1>
+      <Link href="/">Ir para Home</Link>
+    </div>
+  );
+}
---
+++ pages/index.js
@@ -0,0 +1,10 @@
+import Link from "../src/components/Link";
+
+export default function HomePage() {
+  return (
+    <div>
+      <h1>Teste</h1>
+      <Link href="/faq">Ir para Home</Link>
+    </div>
+  );
+}
